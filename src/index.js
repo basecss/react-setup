39967a74b0b4127365b3e9009a4a05d107e26509
basecss <i@basecss.net>
2019-06-16 05:29:08 +0800

    add styled-components

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@
 import React from 'react'
 /* eslint-enable no-unused-vars */
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
 
 import Select from './components/Select'
 import './index.less'
+
+const Button = styled.button`
+  background-color: #ff6600;
+  color: #ffffff;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: none;
+  border-radius: 3px;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in;
+  :hover {
+    background-color: #ff5500;
+  }
+`
 
 const container = document.querySelector('#container')
 const options = [
@@ -15,6 +31,9 @@ const options = [
 ]
 
 ReactDOM.render(
-  <Select items={ options } />,
+  <div>
+    <Button primary>Styled Components</Button>
+    <Select items={ options } />
+  </div>,
   container
 )
